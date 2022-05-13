bae9bd39b524af6c9327cc166a44ac9b0b7efc6a
Arash <arash3908@gmail.com>
2022-05-13 16:25:15 +0430

    Added initial codes for project

---
+++ app/src/main/java/com/example/cpsproject/DisplayMessageActivity.java
@@ -0,0 +1,69 @@
+package com.example.cpsproject;
+
+import android.content.*;
+import android.hardware.*;
+import android.support.v7.app.*;
+import android.os.*;
+import android.widget.*;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor gyroscopeSensor;
+    private SensorEventListener gyroscopeEventListener;
+
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
+        if(gyroscopeSensor == null){
+            Toast.makeText(this, "The Device has no Gyroscope", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        gyroscopeEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                count += 1;
+                if(event.values[2] > 0.5f){
+//                    getWindow().getDecorView().setBackgroundColor(Color.BLUE);
+                    textView.setText(String.valueOf(event.values[2]));
+                }
+                else if(event.values[2] < -0.5f){
+//                    getWindow().getDecorView().setBackgroundColor(Color.YELLOW);
+                    textView.setText(String.valueOf(event.values[2]));
+                }
+                else {
+                    textView.setText(String.valueOf(count));
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(gyroscopeEventListener, gyroscopeSensor, 3);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(gyroscopeEventListener);
+    }
+}
